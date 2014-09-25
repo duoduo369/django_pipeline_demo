cfecbe2ded011377ed63be3b263eaf1d4ccf1c94
yangyang <yangyang@xuetangx.com>
2014-09-25 11:25:35 +0800

    init project

---
+++ app/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+    url(r'^$', 'app.views.index', name='index'),
+)
---
+++ djangomako_demo/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djangomako_demo.views.home', name='home'),
+    # url(r'^djangomako_demo/', include('djangomako_demo.foo.urls')),
+    url(r'', include('app.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
